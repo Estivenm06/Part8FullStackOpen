2b975a21185bdc4f649b4b438c72a25a50f7fa26
LUIGIWONKA <mejiaguetoestiven@gmail.com>
2024-09-17 19:47:15 -0500

    Exercise 8.19 Done

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,8 +1,56 @@
-const Books = ({books}) => {
+import { useState } from "react";
+
+const GenreSet = ({ setGenre }) => {
+  return (
+    <div>
+      <button
+        value={"refactoring"}
+        onClick={({ target }) => setGenre(target.value)}
+      >
+        refactoring
+      </button>
+      <button value={"agile"} onClick={({ target }) => setGenre(target.value)}>
+        agile
+      </button>
+      <button
+        value={"patterns"}
+        onClick={({ target }) => setGenre(target.value)}
+      >
+        patterns
+      </button>
+      <button value={"design"} onClick={({ target }) => setGenre(target.value)}>
+        design
+      </button>
+      <button value={"crime"} onClick={({ target }) => setGenre(target.value)}>
+        crime
+      </button>
+      <button
+        value={"classig"}
+        onClick={({ target }) => setGenre(target.value)}
+      >
+        classic
+      </button>
+      <button
+        value={"all genres"}
+        onClick={({ target }) => setGenre(target.value)}
+      >
+        all genres
+      </button>
+    </div>
+  );
+};
+
+const Books = ({ books }) => {
+  let [genre, setGenre] = useState("all genres");
+  if (genre !== "all genres") {
+    books = books.filter((book) => book.genres == genre);
+  } else {
+    books = books;
+  }
   return (
     <div>
       <h2>books</h2>
-
+      in genre <strong>{genre !== null ? genre : null}</strong>
       <table>
         <tbody>
           <tr>
@@ -19,6 +67,9 @@ const Books = ({books}) => {
           ))}
         </tbody>
       </table>
+      <div>
+        <GenreSet setGenre={setGenre} />
+      </div>
     </div>
   );
 };
